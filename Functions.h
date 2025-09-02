30a3a4b3e02f0cd9f0adc91933099d652fad23a1
SergioAra <47898195+SergioAra@users.noreply.github.com>
2025-09-02 10:25:04 -0500

    Update Functions.h

--- Functions.h
+++ Functions.h
@@ -1,2 +1,26 @@
+USTRUCT(BlueprintType)
+struct FGridData
+{
+	GENERATED_USTRUCT_BODY()
+
+public:
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	int32 CellIndex = -1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FBox Cell;
+
+	FGridData()
+	{
+		Cell = FBox();
+	}
+
+	FGridData(const FVector& InCellLocation, const FVector& InCellExtent)
+	{
+		Cell = FBox(InCellLocation - InCellExtent, InCellLocation + InCellExtent);
+	}
+};
+
 UFUNCTION(BlueprintCallable)
 static void GenerateGrid(const FVector& GridLocation, const FVector& GridExtent, int32 XDivisions, int32 YDivisions, TArray<FGridData>& OutGridCells);
